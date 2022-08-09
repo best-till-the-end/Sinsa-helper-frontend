a9dd56a6a5f1da421f3a9b2a0e1b25197ac0905a
sdwoo <damjang67@naver.com>
2022-08-10 00:09:21 +0900

    git test

--- src/App.js
+++ src/App.js
@@ -7,21 +7,22 @@ import Search from './components/search/Search';
 import SearchResult from './components/search/SearchResult';
 import AdminProducts from './components/search/AdminProducts';
 import MyPage from './components/MyPage';
+// test
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/auth/kakao/login" element={<KakaoAuthTokenHandler />} />
-        <Route path="/search" element={<Search />} />
+        <Route path='/' element={<Main />} />
+        <Route path='/auth/kakao/login' element={<KakaoAuthTokenHandler />} />
+        <Route path='/search' element={<Search />} />
         <Route
-          path="/search/result/:main/:sub/:delivery/:size/:quality"
+          path='/search/result/:main/:sub/:delivery/:size/:quality'
           element={<SearchResult />}
         ></Route>
-        <Route path="/adminProduct" element={<AdminProducts />}></Route>
-        <Route path="/myPage" element={<MyPage />}></Route>
+        <Route path='/adminProduct' element={<AdminProducts />}></Route>
+        <Route path='/myPage' element={<MyPage />}></Route>
       </Routes>
       <GlobalStyle />
     </BrowserRouter>
